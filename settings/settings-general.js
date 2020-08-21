0c0c2890ae04d60b0b25fa58242df9161596d428
csabourin <christian.sabourin@canada.ca>
2020-08-21 13:57:01 -0400

    Final content modifications

--- settings/settings-general.js
+++ settings/settings-general.js
@@ -7,7 +7,8 @@ define({
   "courseSubtitle_fr": "à l’appui des programmes et des services",
   "seriesTitle_en": "",
   "seriesTitle_fr": "",
-  "addToolboxPage": [{ "name_en":"Glossary", "name_fr":"Glossaire", "filename_en":"glossary_en", "filename_fr":"glossary_fr"}],
+  "addToolboxPage": [{ "name_en":"Glossary", "name_fr":"Glossaire", "filename_en":"glossary_en", "filename_fr":"glossary_fr"},
+  { "name_en":"Classroom Activities", "name_fr":"Vers la salle de classe", "filename_en":"classroom_activities_en", "filename_fr":"classroom_activities_fr"}],
 	
 	showFavorites: false,
   "editMode": false
